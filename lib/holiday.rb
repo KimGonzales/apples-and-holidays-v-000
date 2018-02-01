d472f246c5434c4c423a836c4615ee62dba2fe08
Kim Gonzales <kimgo589@gmail.com>
2018-02-01 18:42:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do | holiday, supply|
     supplies << supply
   end
-  supply
+  supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
